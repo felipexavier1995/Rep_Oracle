bcbef2e86a176fe345daa5a0fb2640949233b557
Felipe Bento Xavier <52262336+felipexavier1995@users.noreply.github.com>
2024-10-18 12:47:02 -0300

    Create script_PDB's_and_backup.sql

---
+++ script_PDB's_and_backup.sql
@@ -0,0 +1,57 @@
+--- Some commands of PBDs, container name, alter databases and execute backup (RMAN)
+
+
+/*
+-- command for showner PDB_container name.
+
+show con_name;
+
+*/
+---------------------------------------------------------------------------------------------------------------
+/*
+-- command for showner all PDBS.
+
+show pdbs;
+
+*/
+---------------------------------------------------------------------------------------------------------------
+/*
+--- command for entre in backup tools the oracle database(PS: execute for system operacional)
+
+rman target/
+
+*/
+---------------------------------------------------------------------------------------------------------------
+/*
+--- command shutdown database.
+
+shutdown immediate;
+
+*/
+---------------------------------------------------------------------------------------------------------------
+/*
+--- command for start database.
+
+startup database;
+or
+startup mount;
+
+*/
+---------------------------------------------------------------------------------------------------------------
+/*
+--- command for alter database (alter on OPEN)
+
+alter database open;
+
+*/
+---------------------------------------------------------------------------------------------------------------
+/*
+--- command for execute all backup and start backup (for RMAN tools)
+
+backups archivelog all;
+
+backup database;
+*/
+
+
+
