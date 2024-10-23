a096d09bb7f955190c1e1f5914b60b576928cc9e
Felipe Bento Xavier <52262336+felipexavier1995@users.noreply.github.com>
2024-10-23 09:03:15 -0300

    Create Script_enter_ASM.sql

---
+++ Script_enter_ASM.sql
@@ -0,0 +1,10 @@
+--- Commands where we're can use in only environment ASM.
+
+----------------------------------------------------------------------------------------------
+--- command to enter ASM.
+
+-- before, we need set our instance with command . oraenv and after write our database.
+
+sqlplus / as sysasm
+
+----------------------------------------------------------------------------------------------
