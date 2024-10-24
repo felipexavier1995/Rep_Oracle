aa4a4fdf83bb0da91d27b4b7ceb40a980bf72a8c
Felipe Bento Xavier <52262336+felipexavier1995@users.noreply.github.com>
2024-10-24 14:54:13 -0300

    Create script_about_job.sql

---
+++ script_about_job.sql
@@ -0,0 +1,5 @@
+-- command for alter any job
+--check, drop and view Jobs.
+
+----------------------------------------------------------------------------------------------------------
+-- Checking any job if it's enable.
