27448f9cb89b5898048eb6b4cd82ab6d30a90393
Felipe Bento Xavier <52262336+felipexavier1995@users.noreply.github.com>
2024-10-29 09:05:57 -0300

    Update script_about_job.sql

--- script_about_job.sql
+++ script_about_job.sql
@@ -3,3 +3,10 @@
 
 ----------------------------------------------------------------------------------------------------------
 -- Checking any job if it's enable.
+clear col
+set lines 200 pages 200
+col OWNER for a10
+col JOB_NAME for a40
+col ENABLED for a10
+select OWNER,JOB_NAME, ENABLED, CLIENT_ID from dba_scheduler_jobs where job_name like ('%insert_data%'); -- Here we can swith that name.
+----------------------------------------------------------------------------------------------------------
