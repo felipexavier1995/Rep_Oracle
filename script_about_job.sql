f3d0507fd69b3957df7def4050b6c584ed8a6009
Felipe Bento Xavier <52262336+felipexavier1995@users.noreply.github.com>
2024-11-03 09:40:28 -0300

    Update script_about_job.sql

--- script_about_job.sql
+++ script_about_job.sql
@@ -1,7 +1,7 @@
 -- command for alter any job
 --check, drop and view Jobs.
 
-----------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Checking any job if it's enable.
 clear col
 set lines 200 pages 200
@@ -9,4 +9,32 @@ col OWNER for a10
 col JOB_NAME for a40
 col ENABLED for a10
 select OWNER,JOB_NAME, ENABLED, CLIENT_ID from dba_scheduler_jobs where job_name like ('%insert_data%'); -- Here we can swith that name.
-----------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Checking if a job stoped
+set lines 200 pages 200
+col owner for a15
+col log_date for a35
+col job_name for a20
+col start_date for a20
+col repeat_interval for a20
+col last_start_date for a20
+col next_run_date for a20
+col status for a10
+select log_date, owner, job_name, status
+from dba_scheduler_job_log
+where job_name like '%JOB_INSERIR_DADO%' and status in ('STOPPED');
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Checking of a job stated
+  set lines 200 pages 200
+col owner for a15
+col log_date for a35
+col job_name for a20
+col start_date for a20
+col repeat_interval for a20
+col last_start_date for a20
+col next_run_date for a20
+col status for a10
+select log_date, owner, job_name, status
+from dba_scheduler_job_log
+where job_name like '%JOB-EM-QUESTAO%' and status in ('START');
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
